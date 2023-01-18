5ea901f6a7288a18f54f807787a4345124f15966
Ardemium <ArthurDjamardzhashvili@outlook.com>
2023-01-18 20:48:49 +0100

    added a authentication and authorization

---
+++ code/auth/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,118 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _config;
+
+        public ApplicationUsersController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
+        }
+
+        // POST: api/ApplicationUsers/register
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] ApplicationUserDTO applicationUserDTO)
+        {
+            var user = new ApplicationUser { UserName = applicationUserDTO.UserName, Email = applicationUserDTO.Email };
+
+            var result = await _userManager.CreateAsync(user, applicationUserDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            await _signInManager.SignInAsync(user, isPersistent: false);
+            return Ok();
+        }
+
+        // POST: api/ApplicationUsers/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] ApplicationUserDTO applicationUserDTO)
+        {
+            var user = await _userManager.FindByEmailAsync(applicationUserDTO.Email);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, applicationUserDTO.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+        new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+    };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetValue<string>("JWT:SigningKey")));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _config.GetValue<string>("JWT:Issuer"),
+                _config.GetValue<string>("JWT:Issuer"),
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
+        // PUT: api/ApplicationUsers/modify
+        [HttpPut("modify")]
+        public async Task<IActionResult> Modify([FromBody] ApplicationUserDTO applicationUserDTO)
+        {
+            var user = await _userManager.FindByEmailAsync(applicationUserDTO.Email);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _userManager.ChangePasswordAsync(user, applicationUserDTO.Password, applicationUserDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok();
+        }
+
+        // DELETE: api/ApplicationUsers/delete
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete([FromBody] ApplicationUserDTO applicationUserDTO)
+        {
+            var user = await _userManager.FindByEmailAsync(applicationUserDTO.Email);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ code/auth/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Add services to the container.
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
